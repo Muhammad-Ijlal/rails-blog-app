bf918e5ea309b52c1455ba4f30a99b48ab67b682
Muhammad-Ijlal <mijlalckl@gmail.com>
2022-07-25 15:52:46 +0500

    Added user validations and relationship with article

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
+# Class user
 class User < ApplicationRecord
-  # validates :namename, presence: true, length: { minimum: 6, maximum: 100 }
-  # validates :email, presence: true, length: { minimum: 10, maximum: 300 }  
+  has_many :articles
+  validates :username, presence: true,
+                       uniqueness: { case_sensitive: false },
+                       length: { minimum: 3, maximum: 25 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false },
+                    length: { maximum: 105 },
+                    format: { with: VALID_EMAIL_REGEX }
 end
